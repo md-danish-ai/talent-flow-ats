c73f063cf4c1ff8beb89be2c92f06b31f8dbc33f
varsha24-ag <varsha.nagda@arcgate.com>
2026-02-19 16:03:16 +0530

    create admin api created

--- backend/app/auth/schemas.py
+++ backend/app/auth/schemas.py
@@ -58,3 +58,24 @@ class SignInSchema(BaseModel):
         if not re.match(r"^[0-9]{10}$", value):
             raise ValueError("Password must be 10 digits")
         return value
+
+
+class CreateAdminSchema(BaseModel):
+    name: str
+    mobile: str
+    email: Optional[EmailStr] = None
+
+    @validator("name")
+    def validate_full_name(cls, value):
+        value = value.strip()
+        if len(value.split()) < 2:
+            raise ValueError("Name must be full name (first and last)")
+        if not re.match(r"^[A-Za-z ]+$", value):
+            raise ValueError("Name must contain only letters")
+        return value
+
+    @validator("mobile")
+    def validate_mobile(cls, value):
+        if not re.match(r"^[0-9]{10}$", value):
+            raise ValueError("Mobile must be 10 digits")
+        return value
