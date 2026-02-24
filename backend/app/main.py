baecb9bb811517f4b057ed6b4fa3dde2f44cdafd
md-danish-ai <mohammed.danish@arcgate.com>
2026-02-24 17:14:23 +0530

    remove testing port

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,8 +27,6 @@ origins = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
-    "http://localhost:3002",
-    "http://127.0.0.1:3002",
 ]
 
 app.add_middleware(
